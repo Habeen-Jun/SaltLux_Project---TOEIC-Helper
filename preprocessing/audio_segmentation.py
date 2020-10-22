a20bf8fc609c940a061bf7869d8b2ad2254926d9
Habeen-Jun <61823940+Habeen-Jun@users.noreply.github.com>
2020-10-22 17:17:49 +0900

    Update audio_segmentation.py
    Make ToeicAudioProcessor  class

--- preprocessing/audio_segmentation.py
+++ preprocessing/audio_segmentation.py
@@ -1,119 +1,69 @@
-# importing libraries 
-import speech_recognition as sr 
-  
-import os 
-  
-from pydub import AudioSegment 
-from pydub.silence import split_on_silence 
-  
-# a function that splits the audio file into chunks 
-# and applies speech recognition 
-def silence_based_conversion(path = "alice-medium.wav"): 
-  
-    # open the audio file stored in 
-    # the local system as a wav file. 
-    song = AudioSegment.from_wav(path) 
-  
-    # open a file where we will concatenate   
-    # and store the recognized text 
-    fh = open("recognized.txt", "w+") 
-          
-    # split track where silence is 0.5 seconds  
-    # or more and get chunks 
-    chunks = split_on_silence(song, 
-        # must be silent for at least 0.5 seconds 
-        # or 500 ms. adjust this value based on user 
-        # requirement. if the speaker stays silent for  
-        # longer, increase this value. else, decrease it. 
-        min_silence_len = 500, 
-  
-        # consider it silent if quieter than -16 dBFS 
-        # adjust this per requirement 
-        silence_thresh = -16
-    ) 
-  
-    # create a directory to store the audio chunks. 
-    try: 
-        os.mkdir('audio_chunks') 
-    except(FileExistsError): 
+import librosa
+import librosa.display
+import soundfile as sf
+import matplotlib.pyplot as plt
+import IPython.display as ipd
+import numpy as np
+import os
+import time
+from os import path
+# from pydub import AudioSegment
+
+class ToeicAudioProcessor:
+    def __init__(self):
         pass
-  
-    # move into the directory to 
-    # store the audio files. 
-    os.chdir('audio_chunks') 
-  
-    i = 0
-    # process each chunk 
-    for chunk in chunks: 
-              
-        # Create 0.5 seconds silence chunk 
-        chunk_silent = AudioSegment.silent(duration = 10) 
-  
-        # add 0.5 sec silence to beginning and  
-        # end of audio chunk. This is done so that 
-        # it doesn't seem abruptly sliced. 
-        audio_chunk = chunk_silent + chunk + chunk_silent 
-  
-        # export audio chunk and save it in  
-        # the current directory. 
-        print("saving chunk{0}.wav".format(i)) 
-        # specify the bitrate to be 192 k 
-        audio_chunk.export("./chunk{0}.wav".format(i), bitrate ='192k', format ="wav") 
-  
-        # the name of the newly created chunk 
-        filename = 'chunk'+str(i)+'.wav'
-  
-        print("Processing chunk "+str(i)) 
-  
-        # get the name of the newly created chunk 
-        # in the AUDIO_FILE variable for later use. 
-        file = filename 
-  
-        # create a speech recognition object 
-        r = sr.Recognizer() 
-  
-        # recognize the chunk 
-        with sr.AudioFile(file) as source: 
-            # remove this if it is not working 
-            # correctly. 
-            r.adjust_for_ambient_noise(source) 
-            audio_listened = r.listen(source) 
-  
-        try: 
-            # try converting it to text 
-            rec = r.recognize_google(audio_listened) 
-            # write the output to the file. 
-            fh.write(rec+". ") 
-  
-        # catch any errors. 
-        except sr.UnknownValueError: 
-            print("Could not understand audio") 
-  
-        except sr.RequestError as e: 
-            print("Could not request results. check your internet connection") 
-  
-        i += 1
-  
-    os.chdir('..') 
-  
+
+    def load(self, file_path, sr=22050, time_it=False):
+        if time_it == True:
+            start = time.time()
+            y, sr = librosa.load(file_path, sr=sr)
+            print(time.time() - start)
+        else:
+            y, sr = librosa.load(file_path, sr=sr)
+        return y, sr
+
+    def split_on_silence(self, y, top_db=60):
+        nonMuteSections = librosa.effects.split(y, top_db=top_db)
+        return nonMuteSections
+
+    def save_audio(self,audio, path, sample_rate=22050):
+        audio *= 32767 / max(0.01, np.max(np.abs(audio)))
+        sf.write(path, audio.astype(np.int16), samplerate=sample_rate)
+        print(" [*] Audio saved: {}".format(path))
+
+    def save_audio_files(self, y, save_folder, filename_pattern, nonMuteSections, min_concat_seconds=1.5, padding = 100,
+                         sr = 22050):
+        os.makedirs(save_folder, exist_ok=True)
+        n_concat_signals = sr * min_concat_seconds
+        for idx, i in enumerate(nonMuteSections):
+            sent_len = i[1] - i[0]
+            if sent_len <= n_concat_signals:
+                nonMuteSections[idx + 1][0] = i[0]
+            else:
+                if i[0] != 0:
+                    self.save_audio(y[i[0] - padding:i[1] + padding],
+                               path=os.path.join(save_folder + filename_pattern + str(idx + 1)) + '.wav', sample_rate=sr)
+                else:
+                    self.save_audio(y[i[0]:i[1] + padding], path=os.path.join(save_folder + filename_pattern + str(idx + 1)) + '.wav',sample_rate=sr)
+
+    # def mp3_to_wav(self, src, dst):
+    #     print(path.exists(src))
+    #     # convert wav to mp3
+    #     sound = AudioSegment.from_mp3(src)
+    #     sound.export(dst, format="wav")
 
 
-def mp3_to_wav(src, dst):
-    from os import path
-    from pydub import AudioSegment
-    print(path.exists(src))
-    # convert wav to mp3                                                            
-    sound = AudioSegment.from_mp3(src)
-    sound.export(dst, format="wav")
 
 
-  
-if __name__ == '__main__': 
 
+if __name__ == '__main__':
 
-    mp3_to_wav('TEST1.mp3', 'test1.wav')
-    # print('Enter the audio file path') 
-  
-    # path = input() 
-  
-    # silence_based_conversion(path) 
+    # instantiate ToeicAudioProcessor instance
+    toeic = ToeicAudioProcessor()
+    # load wav file
+    y, sr = toeic.load('test2.wav')
+    # split sounds on predefined threshold
+    nonMuteSections = toeic.split_on_silence(y,top_db=70)
+    # save splitted files
+    toeic.save_audio_files(y, save_folder='./wavs/',filename_pattern='test', nonMuteSections=nonMuteSections,
+                           min_concat_seconds=1)
